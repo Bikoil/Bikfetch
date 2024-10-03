4207e03391cf4e5bebf880b618190724f98fd95c
Bikoil <bikokhaled13@gmail.com>
2024-10-04 02:15:57 +0300

    A lot of improvements, new variables

--- bikfetch.go
+++ bikfetch.go
@@ -1,36 +1,56 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "runtime"
-  "syscall"
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"syscall"
 )
 
 func main() {
+	var uts syscall.Utsname
+	syscall.Uname(&uts)
 
-  var uts syscall.Utsname
-  syscall.Uname(&uts)
+	osrelease, _ := os.Open("/etc/os-release")
+	defer osrelease.Close()
 
-//  file, _ := os.Open("/etc/os-release")
-  user := os.Getenv("USER")
-  osname := runtime.GOOS
-  kernel:= charsToString(uts.Release[:])
+	var osname string
+	scanner := bufio.NewScanner(osrelease)
 
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), "PRETTY_NAME=") {
+			osname = strings.Trim(strings.TrimPrefix(scanner.Text(), "PRETTY_NAME="), "\"")
+			break
+		} else if osname == "" {
+	            osname = runtime.GOOS
+	          }
 
-  fmt.Println("OS:", osname,
-  "\nKernel:", kernel, 
-  "\nUser:", user)
+	} 
+	user := os.Getenv("USER")
+	kernel := charsToString(uts.Release[:])
+	host, _ := os.Hostname()
+	display := os.Getenv("XDG_CURRENT_DESKTOP")
+
+	fmt.Println("╭───────────────╮",
+		"\nOS —", osname,
+		"\nKernel —", kernel,
+		"\nUser —", user,
+	        "\nHost Machine —", host,
+		"\nWM / DE —", display,
+		"\n╰───────────────╯")
 }
 
 // FUNctions... yaaay
-  func charsToString(ca []int8) string {
-	  s := make([]byte, len(ca))
-	  for i, v := range ca {
-		  if v == 0 {
-			  break
-		  }
-		  s[i] = byte(v)
-	  }
-          return string(s)
-  }
+func charsToString(ca []int8) string {
+	s := make([]byte, len(ca))
+	for i, v := range ca {
+		if v == 0 {
+			break
+		}
+		s[i] = byte(v)
+	}
+	return string(s)
+}
+
