413313d0afab56af090d889b192b7ca5323743d0
Bikoil <bikokhaled13@gmail.com>
2024-10-04 01:09:44 +0300

    Expanded the script to grab kernel version

--- bikfetch.go
+++ bikfetch.go
@@ -4,14 +4,33 @@ import (
   "fmt"
   "os"
   "runtime"
+  "syscall"
 )
 
 func main() {
+
+  var uts syscall.Utsname
+  syscall.Uname(&uts)
+
+//  file, _ := os.Open("/etc/os-release")
   user := os.Getenv("USER")
   osname := runtime.GOOS
-  fullosname :=
+  kernel:= charsToString(uts.Release[:])
 
 
-  fmt.Println("OS:", osname, 
+  fmt.Println("OS:", osname,
+  "\nKernel:", kernel, 
   "\nUser:", user)
 }
+
+// FUNctions... yaaay
+  func charsToString(ca []int8) string {
+	  s := make([]byte, len(ca))
+	  for i, v := range ca {
+		  if v == 0 {
+			  break
+		  }
+		  s[i] = byte(v)
+	  }
+          return string(s)
+  }
